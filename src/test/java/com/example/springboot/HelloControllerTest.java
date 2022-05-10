6d0260b21ab347f91ea22e59c7a6cfda61c8eb4d
davlloyd <davidlloyd0@gmail.com>
2022-05-11 09:37:03 +1000

    added test capability

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,21 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+		StringBuilder buildpage = new StringBuilder();
+		buildpage.append("<br><br>");
+		buildpage.append("<center>");
+		buildpage.append("<br><br><center><h1>G'day from <b>TAP</b> + <b>Tanzu</b>! <h1>");
+		buildpage.append("<img src='https://dtb5pzswcit1e.cloudfront.net/assets/images/product_logos/icon_tanzugemfire_1594695582.png' width='32' height='32'>");
+		buildpage.append("</center>");
+		buildpage.append("<br><br>");
+		buildpage.append("<center><img src='https://www.visuartists.com/images/pics/1035_6cHgfp_giphy-23gif.gif' width='150' height='150'></center>");
+		String webpage = buildpage.toString();
+
+        assertEquals(webpage, controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
+            .andExpect(content().string(webpage));
     }
 }
